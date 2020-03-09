c19ba931a6ed522ad64031d2886f82f4bc8f24e3
Bijay karki <c-bijay.karki@charter.com>
2020-03-08 23:08:49 -0600

    Folder Path changed for Test Runner class after Local git folder creation.

--- MediaSolutions_Automation/src/main/java/Cucumber_Test_Runner/TestRunner.java
+++ MediaSolutions_Automation/src/main/java/Cucumber_Test_Runner/TestRunner.java
@@ -8,7 +8,8 @@ import cucumber.api.junit.Cucumber;
  
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="/Users/p2815492/eclipse-workspace/MediaSolutions_Automation/src/main/java/Features/LogIn.feature"
+		//features="/Users/p2815492/eclipse-workspace/MediaSolutions_Automation/src/main/java/Features/LogIn.feature"
+		features="/Users/p2815492/git/MediaSolutionsRepo/MediaSolutions_Automation/src/main/java/Features/Login.feature"
 		, glue= {"StepDefinitions"}
  )
 
--- MediaSolutions_Automation/src/main/java/Cucumber_Test_Runner/TestRunner_SSO.java
+++ MediaSolutions_Automation/src/main/java/Cucumber_Test_Runner/TestRunner_SSO.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="/Users/p2815492/eclipse-workspace/MediaSolutions_Automation/src/main/java/Features/SSOSignUP.feature"
+		features="/Users/p2815492/git/MediaSolutionsRepo/MediaSolutions_Automation/src/main/java/Features/SSOSignUP.feature"
 		, glue= {"StepDefinitions"}
  )
 
